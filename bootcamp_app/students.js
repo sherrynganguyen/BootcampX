dd757a1877a69f7b943d48e65f0e1a6f773eebb0
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-12 23:34:32 +0000

    Connect PostgreSQL database from JavaScript

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,11 +8,15 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id as id, students.name as name , cohorts.name as cohort_name
 FROM students
-LIMIT 5;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '${process.argv[2]}%'
+LIMIT ${process.argv[3]};
 `)
 .then(res => {
-  console.log(res);
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  })
 })
 .catch(err => console.error('query error', err.stack));
